68315e1358c323b4e2c600e9ab093c14f9975b32
Tân Võ <tan.vodl@gmail.com>
2021-09-22 15:04:35 +0700

    Contentful API success

--- pages/blogposts/[slug].js
+++ pages/blogposts/[slug].js
@@ -1,12 +1,15 @@
 import { createClient } from "contentful"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { BLOCKS, INLINES } from "@contentful/rich-text-types";
 import Image from "next/image"
 
+//Access Contenful Space
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_KEY,
 })
 
+//Get content for specify link
 export const getStaticPaths = async () => {
   const res = await client.getEntries({
     content_type: 'blogPost'
@@ -24,6 +27,57 @@ export const getStaticPaths = async () => {
   }
 }
 
+//Render Contentful Richtext fields
+const renderOptions = {
+  renderNode: {
+    [INLINES.EMBEDDED_ENTRY]: (node, children) => {
+      // target the contentType of the EMBEDDED_ENTRY to display as you need
+      if (node.data.target.sys.contentType.sys.id === "blogPost") {
+        return (
+          <a href={`/blog/${node.data.target.fields.slug}`}>            {node.data.target.fields.title}
+          </a>
+        );
+      }
+    },
+    [BLOCKS.EMBEDDED_ENTRY]: (node, children) => {
+      // target the contentType of the EMBEDDED_ENTRY to display as you need
+      if (node.data.target.sys.contentType.sys.id === "codeBlock") {
+        return (
+          <pre>
+            <code>{node.data.target.fields.code}</code>
+          </pre>
+        );
+      }
+
+      if (node.data.target.sys.contentType.sys.id === "videoEmbed") {
+        return (
+          <iframe
+            src={node.data.target.fields.embedUrl}
+            height="100%"
+            width="100%"
+            frameBorder="0"
+            scrolling="no"
+            title={node.data.target.fields.title}
+            allowFullScreen={true}
+          />
+        );
+      }
+    },
+
+    [BLOCKS.EMBEDDED_ASSET]: (node, children) => {
+      // render the EMBEDDED_ASSET as you need
+      return (
+        <img
+          src={`https://${node.data.target.fields.file.url}`}
+          height={node.data.target.fields.file.details.image.height}
+          width={node.data.target.fields.file.details.image.width}
+          alt={node.data.target.fields.description}
+        />
+      );
+    },
+  },
+};
+
 export async function getStaticProps({ params }) {
   const {items} = await client.getEntries ({
     content_type: 'blogPost',
@@ -33,9 +87,9 @@ export async function getStaticProps({ params }) {
   return {
     props: { post: items[0] }
   }
-
 }
 
+//Export content
 export default function BlogDetails({ post }) {
   console.log(post)
   const { featureImage, title, description, content} = post.fields
@@ -53,9 +107,12 @@ export default function BlogDetails({ post }) {
         <h3> {description} </h3>
       </div> 
       <div className="content">
-        <div> {documentToReactComponents(content)} </div>
+        <div className="richtext"> {documentToReactComponents(content,renderOptions)} </div>
       </div>
       <style jsx>{`
+        .richtext {
+          display: block;
+        }
         h2,h3 {
           text-transform: uppercase;
         }
